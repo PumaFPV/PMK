2e53faa2e4ee9307a83c01d068eeda78ffa77b3c
Puma_WS <49276509+PumaFPV@users.noreply.github.com>
2022-11-08 00:44:14 +0100

    First commit

---
+++ Software/PKB_dongle/src/main.cpp
@@ -0,0 +1,16 @@
+#include <Arduino.h>
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() 
+{
+  //Receive ESP-NOW packet
+
+  //Figure out which command it is
+
+  //If special key (ctrl / shift / other) than choose correct kb layout
+
+  //if USB command  -> send 
+}
---
+++ Software/PKB_keyboard/src/main.cpp
@@ -0,0 +1,18 @@
+#include <Arduino.h>
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() 
+{
+  //Read SPI from shift register
+
+  //Send all pressed keys to packet
+
+  //send packet
+
+  //Deal with LED
+
+  //Deal with other stuff
+}
