5face559fcbe161c1a671b80a7a5edd05484651c
Nicolas de Grave <nicolas18090@gmail.com>
2022-12-02 13:26:50 +0100

    bon euh ça marche pas

--- Software/PKB_dongle/lib/pmk/pmk.h
+++ Software/PKB_dongle/lib/pmk/pmk.h
@@ -81,3 +81,127 @@ typedef struct serialStruct {
     const uint8_t packetType = 9;
     uint8_t packet[8];
 }   serialStruct;
+
+// keyboardStruct  convertPacket2Keyboard(packetStruct packet);
+// mouseStruct     convertPacket2Mouse(packetStruct packet);
+// gamepadStruct   convertPacket2Gamepad(packetStruct packet);
+// ledStruct       convertPacket2Led(packetStruct packet);
+// knobStruct      convertPacket2Knob(packetStruct packet);
+// actuatorStruct  convertPacket2Actuator(packetStruct packet);
+// displayStruct   convertPacket2Display(packetStruct packet);
+// telemetryStruct convertPacket2Telemetry(packetStruct packet);
+// serialStruct    convertPacket2Serial(packetStruct packet);
+
+packetStruct receivedPacket;
+
+keyboardStruct keyboardPacket;
+mouseStruct mousePacket;
+gamepadStruct gamepadPacket;
+ledStruct ledPacket;
+knobStruct knobPacket;
+actuatorStruct actuatorPacket;
+displayStruct displayPacket;
+telemetryStruct telemetryPacket;
+serialStruct serialPacket;
+
+keyboardStruct convertPacket2Keyboard(packetStruct packet)
+{
+    keyboardPacket.deviceID = packet.deviceID;
+    keyboardPacket.key[0] = packet.data[0];
+    keyboardPacket.key[1] = packet.data[1];
+    keyboardPacket.key[2] = packet.data[2];
+    keyboardPacket.key[3] = packet.data[3];
+    keyboardPacket.key[4] = packet.data[4];
+    keyboardPacket.key[5] = packet.data[5];
+    keyboardPacket.key[6] = packet.data[6];
+    keyboardPacket.key[7] = packet.data[7];
+    return keyboardPacket;
+}
+
+mouseStruct convertPacket2Mouse(packetStruct packet)
+{
+    mousePacket.deviceID = packet.deviceID;
+    mousePacket.x = packet.data[0];
+    mousePacket.y = packet.data[1];
+    mousePacket.w = packet.data[2];
+    mousePacket.p = packet.data[3];
+    mousePacket.k = packet.data[4];
+    return mousePacket;
+}
+
+gamepadStruct convertPacket2Gamepad(packetStruct packet)
+{
+    gamepadPacket.deviceID = packet.deviceID;
+    gamepadPacket.x = packet.data[0];
+    gamepadPacket.y = packet.data[1];
+    gamepadPacket.z = packet.data[2];
+    gamepadPacket.rz = packet.data[3];
+    gamepadPacket.rx = packet.data[4];
+    gamepadPacket.ry = packet.data[5];
+    gamepadPacket.hh = packet.data[6];
+
+    for(uint8_t i = 0; i < 4; ++i)
+    {
+        gamepadPacket.buttons[i] = packet.data[7+i];
+    }
+    return gamepadPacket;
+}
+
+ledStruct convertPacket2Led(packetStruct packet)
+{
+    ledPacket.deviceID = packet.deviceID;
+    ledPacket.function = packet.data[0];
+    ledPacket.red = packet.data[1];
+    ledPacket.green = packet.data[2];
+    ledPacket.blue = packet.data[3];
+    return ledPacket;
+}
+
+knobStruct convertPacket2Knob(packetStruct packet)
+{
+    knobPacket.deviceID = packet.deviceID;  
+    for(uint8_t i = 0; i < 7; ++i)
+    {
+      knobPacket.knob[i] = packet.data[i];
+    }
+    return knobPacket;
+}
+
+actuatorStruct convertPacket2Actuator(packetStruct packet)
+{
+    actuatorPacket.deviceID = packet.deviceID;
+    actuatorPacket.function = packet.data[0];
+    actuatorPacket.position = packet.data[1];
+    actuatorPacket.command = packet.data[2];
+    return actuatorPacket;
+}
+
+displayStruct convertPacket2Display(packetStruct packet)
+{
+    displayPacket.deviceID = packet.deviceID;
+    displayPacket.image = packet.data[0];
+    displayPacket.x = packet.data[1];
+    displayPacket.y = packet.data[2];
+    displayPacket.brightness = packet.data[3];
+    return displayPacket;
+}
+
+telemetryStruct convertPacket2Telemetry(packetStruct packet)
+{
+    telemetryPacket.deviceID = packet.deviceID;
+    telemetryPacket.battery = packet.data[0];
+    telemetryPacket.temperature = packet.data[1];
+    telemetryPacket.macAddress = packet.data[2];
+    telemetryPacket.error = packet.data[3];
+    return telemetryPacket;
+}   
+
+serialStruct convertPacket2Serial(packetStruct packet)
+{
+    serialPacket.deviceID = packet.deviceID;
+    for(uint8_t i = 0; i < 7; ++i)
+    {
+      serialPacket.packet[i] = packet.data[i];
+    }
+    return serialPacket;
+}
--- Software/PKB_dongle/src/espNowHAndle.h
+++ Software/PKB_dongle/src/espNowHAndle.h
@@ -1,122 +1,8 @@
+#include "pmk.h"
+
 esp_now_peer_info_t peerInfo;
 
 String success;
-
-packetStruct receivedPacket;
-
-keyboardStruct keyboardPacket;
-mouseStruct mousePacket;
-gamepadStruct gamepadPacket;
-ledStruct ledPacket;
-knobStruct knobPacket;
-actuatorStruct actuatorPacket;
-displayStruct displayPacket;
-telemetryStruct telemetryPacket;
-serialStruct serialPacket;
-
-keyboardStruct  convertPacket2Keyboard(packetStruct packet);
-mouseStruct     convertPacket2Mouse(packetStruct packet);
-gamepadStruct   convertPacket2Gamepad(packetStruct packet);
-ledStruct       convertPacket2Led(packetStruct packet);
-knobStruct      convertPacket2Knob(packetStruct packet);
-actuatorStruct  convertPacket2Actuator(packetStruct packet);
-displayStruct   convertPacket2Display(packetStruct packet);
-telemetryStruct convertPacket2Telemetry(packetStruct packet);
-serialStruct    convertPacket2Serial(packetStruct packet);
-
-keyboardStruct convertPacket2Keyboard(packetStruct packet)
-{
-  keyboardPacket.deviceID = packet.deviceID;
-  keyboardPacket.key[0] = packet.data[0];
-  keyboardPacket.key[1] = packet.data[1];
-  keyboardPacket.key[2] = packet.data[2];
-  keyboardPacket.key[3] = packet.data[3];
-  keyboardPacket.key[4] = packet.data[4];
-  keyboardPacket.key[5] = packet.data[5];
-  keyboardPacket.key[6] = packet.data[6];
-  keyboardPacket.key[7] = packet.data[7];
-}
-
-mouseStruct convertPacket2Mouse(packetStruct packet)
-{
-  mousePacket.deviceID = packet.deviceID;
-  mousePacket.x = packet.data[0];
-  mousePacket.y = packet.data[1];
-  mousePacket.w = packet.data[2];
-  mousePacket.p = packet.data[3];
-  mousePacket.k = packet.data[4];
-}
-
-gamepadStruct convertPacket2Gamepad(packetStruct packet)
-{
-  gamepadPacket.deviceID = packet.deviceID;
-  gamepadPacket.x = packet.data[0];
-  gamepadPacket.y = packet.data[1];
-  gamepadPacket.z = packet.data[2];
-  gamepadPacket.rz = packet.data[3];
-  gamepadPacket.rx = packet.data[4];
-  gamepadPacket.ry = packet.data[5];
-  gamepadPacket.hh = packet.data[6];
-  
-  for(uint8_t i = 0; i < 4; ++i)
-  {
-    gamepadPacket.buttons[i] = packet.data[7+i];
-  }
-}
-
-ledStruct convertPacket2Led(packetStruct packet)
-{
-  ledPacket.deviceID = packet.deviceID;
-  ledPacket.function = packet.data[0];
-  ledPacket.red = packet.data[1];
-  ledPacket.green = packet.data[2];
-  ledPacket.blue = packet.data[3];
-}
-
-knobStruct convertPacket2Knob(packetStruct packet)
-{
-  knobPacket.deviceID = packet.deviceID;
-
-  for(uint8_t i = 0; i < 7; ++i)
-  {
-    knobPacket.knob[i] = packet.data[i];
-  }
-}
-
-actuatorStruct convertPacket2Actuator(packetStruct packet)
-{
-  actuatorPacket.deviceID = packet.deviceID;
-  actuatorPacket.function = packet.data[0];
-  actuatorPacket.position = packet.data[1];
-  actuatorPacket.command = packet.data[2];
-}
-
-displayStruct convertPacket2Display(packetStruct packet)
-{
-  displayPacket.deviceID = packet.deviceID;
-  displayPacket.image = packet.data[0];
-  displayPacket.x = packet.data[1];
-  displayPacket.y = packet.data[2];
-  displayPacket.brightness = packet.data[3];
-}
-
-telemetryStruct convertPacket2Telemetry(packetStruct packet)
-{
-  telemetryPacket.deviceID = packet.deviceID;
-  telemetryPacket.battery = packet.data[0];
-  telemetryPacket.temperature = packet.data[1];
-  telemetryPacket.macAddress = packet.data[2];
-  telemetryPacket.error = packet.data[3];
-}
-
-serialStruct convertPacket2Serial(packetStruct packet)
-{
-  serialPacket.deviceID = packet.deviceID;
-  for(uint8_t i = 0; i < 7; ++i)
-  {
-    serialPacket.packet[i] = packet.data[i];
-  }
-}
 
 
 
@@ -133,8 +19,6 @@ void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status)
     success = "Delivery Fail :(";
   }
 }
-
-
 
 // Callback when data is received
 void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
